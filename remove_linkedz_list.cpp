05b7b7720645a8d175ab935f62e95302441c62f3
RohanDobriyal <128817887+RohanDobriyal@users.noreply.github.com>
2024-01-22 00:08:32 +0530

    Add files via upload

---
+++ remove_linkedz_list.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        
+    }
+};
+
+void displayList(ListNode* head) {
+    while (head) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    Solution solution;
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(6);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next->next = new ListNode(6);
+
+    cout << "Original Linked List: ";
+    displayList(head);
+
+    int valToRemove = 6;
+    ListNode* result = solution.removeElements(head, valToRemove);
+
+    std::cout << "Linked List after removing " << valToRemove << ": ";
+    displayList(result);
+
+    while (result) {
+        ListNode* temp = result;
+        result = result->next;
+        delete temp;
+    }
+
+    return 0;
+}
